ed20ac993a11bc2d9da037987a932c14934b27e3
AmirHosseinBiabani <AmirhosseinBiyabani@yahoo.com>
2024-07-17 12:09:48 +0330

    If()s in Cart Done

--- Application.Store/Carts/CreateCart/CreateCartCommandHandler.cs
+++ Application.Store/Carts/CreateCart/CreateCartCommandHandler.cs
@@ -1,13 +1,32 @@
 ﻿using MediatR;
 using Domain.Invoices.Repository;
 using Domain.Invoices;
+using Domain.Users.Repository;
+using Domain.Products.Repository;
+using Domain.Users;
 
 namespace Application.Carts.CreateCart
 {
-    public class CreateCartCommandHandler(ICratRepository repository) : IRequestHandler<GetUserCartCommend, CreateCartCommandResponse>
+    public class CreateCartCommandHandler(ICratRepository repository, IUserReopsitory userReopsitory, IProductRepository productRepository) : IRequestHandler<GetUserCartCommend, CreateCartCommandResponse>
     {
         public async Task<CreateCartCommandResponse> Handle(GetUserCartCommend request, CancellationToken cancellationToken)
         {
+
+            var user = userReopsitory.GetById(request.userid);
+            if (user == null)
+            {
+                throw new NullReferenceException(nameof(request.userid));
+            }
+
+
+            var product = productRepository.GetById(request.validationid);
+            if (product == null)
+            {
+                throw new NullReferenceException(nameof(request.validationid));
+            }
+
+
+
             var cart = CartOrInvoiceDtail.Create(request.userid, request.validationid, request.price, request.saleprice);
 
             var id = repository.Create(cart);
