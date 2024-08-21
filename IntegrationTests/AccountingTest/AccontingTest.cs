548981ef14e351e2979670904073c6fafc93d173
AmirHosseinBiabani <AmirhosseinBiyabani@yahoo.com>
2024-08-21 14:20:54 +0330

    Login_Should_Return_Token_When_Valid_Credentials...............................

--- IntegrationTests/AccountingTest/AccontingTest.cs
+++ IntegrationTests/AccountingTest/AccontingTest.cs
@@ -29,32 +29,33 @@ namespace IntegrationTests.AccountingTest
         {
             builder.ConfigureServices(services =>
             {
-               
+                Console.WriteLine("Configuring WebHost...");
+
                 var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
                 if (descriptor != null)
                 {
                     services.Remove(descriptor);
                 }
 
-              
+
                 services.AddDbContext<ApplicationDbContext>(options =>
                 {
                     options.UseInMemoryDatabase("InMemoryDbForTesting");
                 });
 
-                
+
                 var serviceProvider = services.BuildServiceProvider();
 
-                
+
                 using (var scope = serviceProvider.CreateScope())
                 {
                     var scopedServices = scope.ServiceProvider;
                     var db = scopedServices.GetRequiredService<ApplicationDbContext>();
 
-                
+
                     db.Database.EnsureCreated();
 
-                    
+
                 }
             });
         }
@@ -78,63 +79,58 @@ namespace IntegrationTests.AccountingTest
         [Fact]
         public async Task Register_Should_Return_Ok_When_Valid_Data()
         {
-            var model = _fixture.Build<RegisterViewModel>().Create();
+            // Arrange
+            var model = _fixture.Build<RegisterViewModel>()
+                .With(p => p.Email, "user@e123xsample.com")
+                .With(p => p.Password, "Aa123456")
+                .With(p => p.RePassword, "Aa123456")
+                .With(p => p.Phone, "090333589442")
+                .Create();
 
-        // Arrange
-        //var registerModel = new RegisterViewModel
-        //    {
-        //        Email = "user@e123xsample.com",
-        //        Name = "Alimissdhzjbsads",
-        //        Family = "UserFamily",
-        //        Phone = "090333589442",
-        //        Password = "Aa123456.",
-        //      RePassword = "Aa123456."
-        //};
+
 
             var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
 
             // Act
-           
+
             var response = await _client.PostAsync("/api/account/register", content);
 
             // Assert
-        
+
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
             var responseString = await response.Content.ReadAsStringAsync();
             responseString.Should().NotBeNullOrEmpty();
         }
 
+        [Fact]
+        public async Task Register_Should_Return_BadRequest_When_Model_Is_Not_Walid()
+        {
+            // Arrange
+            var model = _fixture.Build<RegisterViewModel>()
+                .With(p => p.Email, "test")
+                .With(p => p.Password, "b")
+                .With(p => p.RePassword, "a")
+                .With(p => p.Phone, "slam")
+                .Create();
+            var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+            // Act
+
+            var response = await _client.PostAsync("/api/account/register", content);
+
+            // Assert
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+            var responseString = await response.Content.ReadAsStringAsync();
+            responseString.Should().NotBeNullOrEmpty();
+        }
+
+
+
 
         [Fact]
         public async Task Login_Should_Return_Token_When_Valid_Credentials()
         {
-            // Arrange
-            var user = new User
-            {
-                UserName = "user",
-                Family = "UserFamily",
-                Email = "user@example.com",
-                PhoneNumber = "1234567890"
-            };
 
-            var password = "Test@123";
-            await _userManager.CreateAsync(user, password);
-
-            var loginModel = new LoginViewModel
-            {
-                Usename = "user",
-                Password = password
-            };
-
-            var content = new StringContent(JsonConvert.SerializeObject(loginModel), Encoding.UTF8, "application/json");
-
-            // Act
-            var response = await _client.PostAsync("/api/account/login", content);
-
-            // Assert
-            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
-            var responseString = await response.Content.ReadAsStringAsync();
-            responseString.Should().Contain("Token");
         }
     }
 }
